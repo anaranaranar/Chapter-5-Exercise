b00088125f59d1a1b7e107e406f760a06b435bd1
anar <anar@gmail.com>
2023-01-31 13:39:15 -0800

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 
+final int MAX_PRICE = 3000;
+final int MAX_WEIGHT = 60;
+final int MAX_BATTERY = 400;
+
+Scanner input = new Scanner(System.in);
 public class Main {
-    public static void main(String[] args) {
-
-        final int MAX_PRICE = 3000;
-        final int MAX_WEIGHT = 60;
-        final int MAX_BATTERY = 400;
-        Scanner input = new Scanner(System.in);
-
-
-        System.out.println("I'm going to help you choose aan eBIke");
-        System.out.println("Please enter the following for the bike model:");
+    public static void valueCalc();
+    int valueRating;
         System.out.print("How much does it cost?: ");
         int cost = input.nextInt();
         System.out.print("How much does it weigh in pounds?: ");
         int weight = input.nextInt();
         System.out.print("How many watt hours is the battery?: ");
         int battery = input.nextInt();
+        if cost <= 3000
+            System.out.println("That's a good deal");
+        else System.out.println("It's too expensive");
+
+    public static void main(String[] args) {
+
+        System.out.println("I'm going to help you choose aan eBIke");
+        System.out.println("Please enter the following for the bike model:");
+        valueCalc();
+
 
     }
 }
